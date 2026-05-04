a5018fae1848705146c506dbcf41465623c69f46
Soupcan <sudo.toby@outlook.com>
2026-05-04 17:37:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 (function applyTabCloak() {
   try {
     var tabData = JSON.parse(localStorage.getItem('tab') || '{}');
-    if (tabData.title) document.title = tabData.title;
-    if (tabData.icon) {
-      var link = document.querySelector("link[rel='icon']");
-      if (link) link.href = tabData.icon;
+
+    if (tabData.title) {
+      document.title = tabData.title;
     }
-  } catch(e) {}
+
+    if (tabData.icon) {
+      var link = document.querySelector("link[rel*='icon']");
+
+      if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+      }
+
+      link.href = tabData.icon + '?v=' + Date.now();
+    }
+  } catch (e) {}
 })();
